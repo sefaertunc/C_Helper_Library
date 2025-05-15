0ce5d349d502f44619239996e2f1b7465fdb5daa
sefaertnc <sefaertnc@gmail.com>
2025-05-15 16:15:26 +0200

    feat(s_string): add strrev
    Add two types of strrev function

--- sources/string/ch_strrev_copy.c
+++ sources/string/ch_strrev_copy.c
@@ -1,29 +1,32 @@
 /* ******************************************************************** */
 /*                                                                      */
 /*                                                          #           */
-/*   libch.h                                               ###          */
+/*   ch_strrev_copy.c                                      ###          */
 /*                                                        #####         */
 /*   By: sefaertnc <sefaertnc@sgmail.com>                #######        */
 /*                                                      #########       */
-/*   Created: 2025/05/15 16:04:53 by sefaertnc         ###########      */
-/*   Updated: 2025/05/15 16:04:53 by sefaertnc        #############     */
+/*   Created: 2025/05/15 15:56:57 by sefaertnc         ###########      */
+/*   Updated: 2025/05/15 15:56:57 by sefaertnc        #############     */
 /*                                                                      */
 /* ******************************************************************** */
 
-#ifndef LIBCH_H
-# define LIBCH_H
+#include "../../include/libch.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
-# include <ctype.h>
-# include <stdarg.h>
-# include <stdint.h>
-# include <stdbool.h>
-# include <time.h>
-# include <errno.h>
+char	*ch_strrev_copy(char *str)
+{
+	char	*rt_ptr;
+	size_t	str_len;
+	size_t	i;
 
-char	*ch_strrev_inplace(char *str);
-char	*ch_strrev_copy(char *str);
-#endif
+	if (!str)
+		return (NULL);
+	str_len = strlen(str);
+	rt_ptr = (char *)malloc(str_len + 1);
+	if (!rt_ptr)
+		return (NULL);
+	i = 0;
+	while (str_len)
+		rt_ptr[i++] = str[--str_len];
+	rt_ptr[i] = '\0';
+	return (rt_ptr);
+}
--- sources/string/ch_strrev_inplace.c
+++ sources/string/ch_strrev_inplace.c
@@ -1,29 +1,33 @@
 /* ******************************************************************** */
 /*                                                                      */
 /*                                                          #           */
-/*   libch.h                                               ###          */
+/*   ch_strrev_inplace.c                                   ###          */
 /*                                                        #####         */
 /*   By: sefaertnc <sefaertnc@sgmail.com>                #######        */
 /*                                                      #########       */
-/*   Created: 2025/05/15 16:04:53 by sefaertnc         ###########      */
-/*   Updated: 2025/05/15 16:04:53 by sefaertnc        #############     */
+/*   Created: 2025/05/15 15:56:45 by sefaertnc         ###########      */
+/*   Updated: 2025/05/15 15:56:45 by sefaertnc        #############     */
 /*                                                                      */
 /* ******************************************************************** */
 
-#ifndef LIBCH_H
-# define LIBCH_H
+#include "../../include/libch.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
-# include <ctype.h>
-# include <stdarg.h>
-# include <stdint.h>
-# include <stdbool.h>
-# include <time.h>
-# include <errno.h>
+char	*ch_strrev_inplace(char *str)
+{
+	char	temp;
+	size_t	str_len;
+	size_t	i;
 
-char	*ch_strrev_inplace(char *str);
-char	*ch_strrev_copy(char *str);
-#endif
+	if (!str)
+		return (NULL);
+	str_len = strlen(str);
+	i = 0;
+	while (i < str_len / 2)
+	{
+		temp = str[i];
+		str[i] = str[str_len - 1 - i];
+		str[str_len - 1 - i] = temp;
+		++i;
+	}
+	return (str);
+}

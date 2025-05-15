f9d82d54440f17069f9bab2287d6f6592e2cf916
sefaertnc <sefaertnc@gmail.com>
2025-05-15 16:11:47 +0200

    chore(structure): main project structure
    Create folder structure, Add necessary libraries

---
+++ include/libch.h
@@ -0,0 +1,27 @@
+/* ******************************************************************** */
+/*                                                                      */
+/*                                                          #           */
+/*   libch.h                                               ###          */
+/*                                                        #####         */
+/*   By: sefaertnc <sefaertnc@sgmail.com>                #######        */
+/*                                                      #########       */
+/*   Created: 2025/05/15 16:04:53 by sefaertnc         ###########      */
+/*   Updated: 2025/05/15 16:04:53 by sefaertnc        #############     */
+/*                                                                      */
+/* ******************************************************************** */
+
+#ifndef LIBCH_H
+# define LIBCH_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <ctype.h>
+# include <stdarg.h>
+# include <stdint.h>
+# include <stdbool.h>
+# include <time.h>
+# include <errno.h>
+
+#endif
